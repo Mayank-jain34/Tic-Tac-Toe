8b1040a03089a3fb7dd5c4913450621c262d36da
Mayank Jain <mayank_jain1@persistent.com>
2017-07-11 18:54:52 +0530

    added react redux boilerplate code

---
+++ client/containers/LoginContainer.js
@@ -0,0 +1,5 @@
+import { connect } from 'react-redux';
+import Login from '../components/Login';
+
+const LoginContainer = connect()(Login);
+export default LoginContainer;
--- client/index.js
+++ client/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
 import App from './components/App';
 
 const io = require('socket.io-client');
 var socket = io.connect('http://localhost:3000');
 
+const eventEmitter = store => next => action => {
+  if(action.type === 'event') {
+    socket.emit(action.eventName, action.eventData);
+  }
+  return next(action);
+}
+
+let store = createStore(rootReducer, applyMiddleware());
+
+
 render(<App socket={socket} />, document.getElementById('app'));
+
+/*
+<Provider store={store}>
+  <Router history={ browserHistory }>
+    <Route path="/" component={App} />
+  </Router>
+</Provider>
+*/
